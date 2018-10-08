21b6a920e62685f7ceeded654644aef0d95af59b
nohabroha <noahysimon@gmail.com>
2018-10-08 14:45:06 -0500

    set scrape.js

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,39 +1,49 @@
+// imports axios and cheerio
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-
+// scrape function
 var scrape = function() {
+  // uses axios to scrape uncrate
   return axios.get("https://uncrate.com/").then(function(res) {
     var $ = cheerio.load(res.data);
 
-    var articles = [];
+    //array to store products
+    var products = [];
 
-
+  // cheerio to grab articles
     $(".article").each(function(i, element) {
 
+      // scrape head ie headline
       var head = $(this)
         .find("h1")
         .text()
         .trim();
+      // scrape URL 
       var url = $(this)
         .find("a")
         .attr("href");
+      // scrape summary
       var summary = $(this)
         .find("p")
         .text()
         .trim();   
 
-        var dataToAdd = {
-          headline: head,
-          summary: summary,
-          url: url
-        };
+      // scraped data object
+      var dataToAdd = {
+        headline: head,
+        summary: summary,
+        url: url
+      };
 
-        articles.push(dataToAdd);
+      // push dateToAdd object to products
+      products.push(dataToAdd);
       
     });
-    return articles;
+    // return products array
+    return products;
   });
 };
 
+//export scrape
 module.exports = scrape;
